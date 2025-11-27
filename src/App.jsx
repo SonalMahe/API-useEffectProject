28d40d2af2a8b0afbed0d0859b30bfc236c25dc2
Sonal Maheshwari <sonal.obedient@gmail.com>
2025-11-27 08:56:45 +0100

    updated and create structure
    -app.css
    -api url link
    -

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RandomUser from "../src/components/randomUser.jsx"
+
+function App() {
+
+  return (
+    <>
+    <RandomUser></RandomUser>
+    </>
+  )
+}
+
+
+
+export default App;
---
+++ src/components/randomUser.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+
+
+function RandomUser() {
+    const [user, setUser] = useState(null);
+    const getData = async () => {
+        try {
+            const result = await fetch("https://randomuser.me/api/");
+            const apiData = await result.json();
+            setUser(apiData.results[0]);
+        }
+        catch (error) {
+            console.log("error in fetching user info", error);
+        }
+    }
+    const GetUserInfo = () => {
+        getData();
+    }
+    useEffect(() => {
+        if (user) {
+            document.title = `Welcome ${user.name.first} ${user.name.last}`
+        }
+    },[user]);
+    return (
+        <div>
+
+            <h1> User information</h1>
+            <button onClick={GetUserInfo}> Get User</button>
+
+            {/* Display User */}
+            {user && (
+                <div style={{ marginTop: "20px" }}>
+                    <h3> User name: {user.name.first} {user.name.last}</h3>
+                    <h3>Email:  {user.email} </h3>
+                    <h2>Country lives in: {user.location.country}</h2>
+                    Image: <img src={user.picture.large} alt={user.name.first} />
+                    Image: <img src={user.picture.medium} alt={user.name.first} />
+                  
+                    <p></p>
+                </div>
+            )}
+
+
+        </div>
+    );
+};
+
+
+export default RandomUser;
